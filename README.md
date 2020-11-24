800684553fd77bc3a2466e81958aa639c392f66d
Olumide Okedusi <odokedusi@sidmach.com>
2020-11-24 19:55:40 +0100

    Improve styling and deleted redundant files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BuyCoins | Frontend Engineer Assessment
-
+[![Netlify Status](https://api.netlify.com/api/v1/badges/a8746e28-bd22-4263-bd4c-e3d014ac6286/deploy-status)](https://app.netlify.com/sites/buycoins-githubclone/deploys)
 ## Description
 This project is a recreation of my GitHub profile "repositories" tab leveraging the GraphQL GitHub API
 
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { query } from "./query.js";
-import { API_KEY } from "./token.js";
+import { query, API_KEY } from "./config.js";
 
 const data = { query };
 
@@ -40,8 +39,9 @@ fetch(`https://api.github.com/graphql`, {
     navImage.setAttribute("src", apiResponse.avatarUrl);
     role.textContent = apiResponse.bio;
     status.textContent = apiResponse.status.message;
-    statusEmoji.innerHTML = apiResponse.status.emojiHTML;
-    statusEmojiMobile.innerHTML = `${apiResponse.status.emojiHTML} ${apiResponse.status.message}`;
+    statusEmoji.innerHTML =
+      apiResponse.status.emojiHTML + `<span>${apiResponse.status.message}</span>`;
+    statusEmojiMobile.innerHTML = `${apiResponse.status.emojiHTML} <span style="margin-left:3px"> ${apiResponse.status.message}</span>`;
     username.textContent = apiResponse.name;
     handle.textContent = apiResponse.login;
     email.textContent = apiResponse.email;
@@ -67,42 +67,67 @@ const removeAllChildNodes = (parent) => {
 }
 
 const addChildNode = (repo) => {
-  var repository = document.createElement("div");
+  // console.log(repo.languages.nodes[0])
+  let repository = document.createElement("div");
   repository.className = "repository";
 
-  var left = document.createElement("div");
+  let left = document.createElement("div");
   left.className = "left";
 
-  var right = document.createElement("div");
+  let right = document.createElement("div");
   right.className = "right";
 
-  var h3 = document.createElement("h3");
+  let h3 = document.createElement("h3");
   h3.className = "repository__name";
-  var h3TextNode = document.createTextNode(repo.name);
+  let h3TextNode = document.createTextNode(repo.name);
   h3.appendChild(h3TextNode);
 
-  var p = document.createElement("p");
+  let p = document.createElement("p");
   p.className = "repository__desc";
-  var pTextNode = document.createTextNode(repo.description ?? "");
+  let pTextNode = document.createTextNode(repo.description ?? "");
   p.appendChild(pTextNode);
 
-  var small = document.createElement("small");
+  let div = document.createElement("div");
+  div.className = "repository__foot";
+
+  let smallMain = document.createElement("small");
+  smallMain.className = "repository__foot__grp";
+
+  let spanColor = document.createElement("span");
+  spanColor.className = "color__icon";
+  spanColor.style.backgroundColor = repo.languages.nodes[0].color;
+
+  let spanLang = document.createElement("span");
+  spanLang.className = "language";
+  let spanLangTextNode = document.createTextNode(repo.languages.nodes[0].name);
+  spanLang.appendChild(spanLangTextNode);
+
+  smallMain.appendChild(spanColor);
+  smallMain.appendChild(spanLang);
+
+  div.appendChild(smallMain)
+
+
+  let small = document.createElement("small");
   small.className = "updated";
-  var smallTextNode = document.createTextNode(xDaysFromNow(repo.updatedAt));
+  let smallTextNode = document.createTextNode(yDaysFromNow(repo.updatedAt));
   small.appendChild(smallTextNode);
 
-  var i = document.createElement("i")
+  div.appendChild(small);
+
+
+  let i = document.createElement("i");
   i.className = "far fa-star";
 
-  var btn = document.createElement("button");
+  let btn = document.createElement("button");
   btn.className = "star__btn";
-  var btnTextNode = document.createTextNode(` Star`);
+  let btnTextNode = document.createTextNode(` Star`);
   btn.appendChild(i);
   btn.appendChild(btnTextNode);
 
   left.appendChild(h3);
   left.appendChild(p);
-  left.appendChild(small);
+  left.appendChild(div);
 
   right.appendChild(btn);
 
@@ -112,9 +137,31 @@ const addChildNode = (repo) => {
   repositories.appendChild(repository);
 }
 
-const xDaysFromNow = (dateString) => {
-  var today = new Date();
-  var targetDate = new Date(dateString);
-  var timeDiff = today.getTime() - targetDate.getTime();
-  return Math.ceil(timeDiff / (1000 * 60 * 60 * 24)) + " days ago";
-}
+// const xDaysFromNow = (dateString) => {
+//   let today = new Date();
+//   let targetDate = new Date(dateString);
+//   let timeDiff = today.getTime() - targetDate.getTime();
+//   return "Updated " + Math.ceil(timeDiff / (1000 * 60 * 60 * 24)) + " days ago";
+// }
+
+const yDaysFromNow = (dateString) => {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let targetDate = new Date(dateString);
+  let month = targetDate.getMonth();
+  let day = targetDate.getDate();
+  let result = `${months[month]} ${day}`
+  return `Updated on ${result}`;
+};
